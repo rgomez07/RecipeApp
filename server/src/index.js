5b7eedc6495a3b44294d13c3480f1be1b93c0dfc
Rigoberto <rigobertobgomez@gmail.com>
2023-03-31 21:50:34 -0500

    fixed dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('a');
+dotenv.config({ path: './.env' });
+mongoose.connect(process.env.MONGOOSEKEY);
 
 app.listen(1738, () => console.log('Im like hey whats up hello!'));
