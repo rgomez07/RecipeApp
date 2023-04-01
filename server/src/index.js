50005bfcd3d81cabac5d61a19ba92bc6a82a4b0b
Rigoberto <rigobertobgomez@gmail.com>
2023-03-31 21:29:08 -0500

    added server

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect();
+
+app.listen(1738, () => console.log('Im like hey whats up hello!'));
