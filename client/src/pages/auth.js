e74319cd0fc9c8da2129e04350721119e3bfe86a
Rigoberto <rigobertobgomez@gmail.com>
2023-04-04 13:28:48 -0500

    fixed login

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export const Auth = () => {
   return (
@@ -13,6 +15,23 @@ export const Auth = () => {
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [_, setCookies] = useCookies(['access_token']);
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:1738/auth/login', {
+        username,
+        password,
+      });
+      setCookies('access_token', response.data.token);
+      window.localStorage.setItem('userID', response.data.userID);
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Form
@@ -21,6 +40,7 @@ const Login = () => {
       password={password}
       setPassword={setPassword}
       label='Login'
+      onSubmit={onSubmit}
     />
   );
 };
