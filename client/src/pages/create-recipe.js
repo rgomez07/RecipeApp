4f96711b641685079f3e7f96452000d73797efdf
Rigoberto <rigobertobgomez@gmail.com>
2023-04-05 21:58:39 -0500

    added a basic form

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,20 @@
 export const CreateRecipe = () => {
-  return <div>Create Recipe</div>;
+  return (
+    <div className='create-recipe'>
+      <h2>Create Recipe</h2>
+      <form>
+        <label htmlFor='Name'>Name</label>
+        <input type='text' id='name' />
+        <label htmlFor='description'>Description</label>
+        <textarea id='description' name='description' />
+        <label htmlFor='ingredients'>Ingredients</label>
+        <label htmlFor='instructions'>Instructions</label>
+        <textarea id='instructions' name='instructions' />
+        <label htmlFor='imageUrl'>Image URL</label>
+        <input type='text' id='imageUrl' name='imageUrl' />
+        <label htmlFor='cookingTime'>Cooking Time (in minutes)</label>
+        <input type='number' id='cookingTime' name='cookingTime' />
+      </form>
+    </div>
+  );
 };
